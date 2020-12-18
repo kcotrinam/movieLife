f3879707b57834665be1cb799e4aea6d0ced5ef6
Kevin Cotrina <kcotrinam92@gmail.com>
2020-12-18 17:49:13 -0500

    Add category model and controllers. Add views for new controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,7 +11,7 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
     if @category.save
       flash[:success] = 'Category successfully created'
-      redirect_to @category
+      redirect_to categories_path
     else
       flash[:error] = 'Something went wrong'
       render 'new'
