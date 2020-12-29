397f4608cf420c0f0914fb96bf90c6e0e2f036d2
Kevin Cotrina <kcotrinam92@gmail.com>
2020-12-29 10:58:05 -0500

    Change the order of the articles by DESC

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ApplicationRecord
   validates :priority, presence: true, inclusion: 1..5
 
   has_many :artcats, dependent: :destroy
-  has_many :articles, -> { order('created_at asc') }, through: :artcats
+  has_many :articles, -> { order('created_at desc') }, through: :artcats
 
   scope :for_navbar, -> { limit(5) }
 end
