d7b0cf4059de13719f3d5e8c351018bd632d83c4
Kevin Cotrina <kcotrinam92@gmail.com>
2020-12-31 12:09:24 -0500

    Add errors messages to all views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,14 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user
       session[:user_id] = user.id
-      redirect_to root_path, flash: {access: "Successfully logged-in!"}
+      redirect_to root_path, flash: {success: "Successfully logged-in!"}
     else
-      redirect_to sign_in_path, flash: {warning: "Username doesn't exist"}
+      redirect_to sign_in_path, flash: {errors: "Username doesn't exist"}
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'Good bye!'
+    redirect_to root_path, flash: { errors: 'Good bye!' }
   end
 end
